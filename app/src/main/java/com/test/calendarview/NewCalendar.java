6e3e912c6e9bbcf4a9d80ce2fa941fa90ecd1210
dapengyou <ladyztt@sina.com>
2017-11-17 00:17:11 +0800

    搭建项目

---
+++ app/src/main/java/com/test/calendarview/NewCalendar.java
@@ -0,0 +1,24 @@
+package com.test.calendarview;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by lady_zhou on 2017/11/17.
+ */
+
+public class NewCalendar extends LinearLayout {
+    public NewCalendar(Context context) {
+        super(context);
+    }
+
+    public NewCalendar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NewCalendar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
